2b36f11aeccca90680e00cd605a494dadcf14dfe
PTRT11901 <marie.gallardo@capgemini.com>
2020-02-01 21:52:18 +0100

    [FEAT][CALENDAR][26] Ajout d'un squelette de calendrier

--- src/app/shared/calendar/calendar.component.ts
+++ src/app/shared/calendar/calendar.component.ts
@@ -1,14 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CalendarEvent, CalendarView, CalendarDateFormatter } from 'angular-calendar';
+import { CustomDateFormatter } from './calendar-formatter.provider';
 
+const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
+  providers: [
+    {
+      provide: CalendarDateFormatter,
+      useClass: CustomDateFormatter
+    }
+  ]
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+  viewDate: Date = new Date();
 
-  ngOnInit() {}
+  /**
+   * TODO
+   * Importer les events de la base de données
+   * Peut utiliser CalendarEvent ??
+   */
+
+  constructor() {}
+
+  ngOnInit(): void {
+  } 
+
+  dayClicked(event): void {
+    console.log('Je clique sur une date du calendrier = ' + event.date);
+    /**
+     * TODO
+     * Si event : On ouvre une popup avec les events présents en ce jour
+     * (Rappel : la date possède un esthétique particulier)
+     */
+  }
 
 }
