b4151afdb564c396426fa4927780440c94fb94db
Amandine Watrelos <amandine.watrelos@gmail.com>
2020-02-11 21:52:59 +0100

    [FEAT][GROUP-6] ajout du package autocomplete et début de l'implémentation

--- src/app/homepage/groupe/form/group-form.component.ts
+++ src/app/homepage/groupe/form/group-form.component.ts
@@ -1,14 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {AutoCompleteService} from "ionic4-auto-complete";
+import {UserService} from "../../../../services/user.service";
 
 @Component({
   selector: 'app-group-form',
   templateUrl: './group-form.component.html',
   styleUrls: ['./group-form.component.scss'],
 })
-export class GroupFormComponent implements OnInit {
+export class GroupFormComponent implements OnInit, AutoCompleteService {
 
-  constructor() { }
+  constructor(
+      private userService: UserService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.labelAttribute = 'username';
+  }
+
+  formValueAttribute: any;
+  labelAttribute: string;
+
+  getItemLabel(item: any): any {
+  }
+
+  //https://www.npmjs.com/package/ionic4-auto-complete
+
+  getResults(term: any): any {
+    if(!term) {
+      return false;
+    }
+    /*return this.http.get('https://restcountries.eu/rest/v2/name/' + keyword).pipe(map(
+        (result: any[]) => {
+          return result.filter(
+              (item) => {
+                return item.name.toLowerCase().startsWith(
+                    keyword.toLowerCase()
+                );
+              }
+          );
+        }
+    ));*/
+  }
 
 }
