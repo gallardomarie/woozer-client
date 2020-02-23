349bc50d73ff9d220a50e7fa0f9416a91b72bf8e
Amandine <amandine.watrelos@gmail.com>
2020-02-23 23:46:50 +0100

    [FEAT][GENERAL-40] liste des groupes sur page d'accueil

--- src/app/homepage/home/home.component.ts
+++ src/app/homepage/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Group } from '../groupe/group';
+import { GroupService } from 'src/services/group.service';
+import { Router } from '@angular/router';
+import { CacheService } from 'src/services/cache.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  groups: Group[];
 
-  ngOnInit() {}
+  constructor(
+    private groupService: GroupService,
+    private router: Router,
+    private cacheService: CacheService
+  ) { }
+
+  ngOnInit() {
+    //TODO replace with dynamic user id
+    this.groupService.findGroupsByUser('1').then((groups) => {
+      this.groups = groups;
+    });
+  }
+
+  navigateToGroup(idGroup){
+    this.router.navigate(['/woozer/event', {id: idGroup}]);
+    this.cacheService.emitChange(idGroup);
+  }
 
 }
