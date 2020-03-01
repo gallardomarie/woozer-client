f7dacd314b9cf74ce325a245fdfffe87b5dca0b7
Amandine Watrelos <amandine.watrelos@gmail.com>
2020-03-01 17:29:23 +0100

    [FEAT][DISCUSSION-44] ajout scroll vertical sur liste des messages

--- src/app/homepage/discussion/details/discussion-details.component.ts
+++ src/app/homepage/discussion/details/discussion-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {DiscussionService} from '../../../../services/discussion.service';
 import {CacheService} from '../../../../services/cache.service';
@@ -6,6 +6,8 @@ import {Discussion} from '../discussion';
 import { Message } from '../message';
 import { UserService } from 'src/services/user.service';
 import { MessageService } from 'src/services/message.service';
+import {User} from "../../../login/user";
+import {IonContent} from "@ionic/angular";
 
 @Component({
     selector: 'app-discussion-details',
@@ -14,8 +16,12 @@ import { MessageService } from 'src/services/message.service';
 })
 export class DiscussionDetailsComponent implements OnInit {
 
+    @ViewChild(IonContent, null)
+    private content: IonContent;
+
     discussion: Discussion;
     userId: number;
+    user: User;
     message: Message;
 
     constructor(
@@ -32,7 +38,9 @@ export class DiscussionDetailsComponent implements OnInit {
         this.discussionService.findById(+this.router.url[this.router.url.length - 1]).then((discussion) => {
             this.discussion = discussion;
             this.userService.findById(this.userId).then((user) => {
-                this.message = new Message(null, user, new Date(), "");
+                this.user = user;
+                this.message = new Message(null, user, null, "");
+                this.content.scrollToBottom(1000);
             });
         });
     }
@@ -42,9 +50,13 @@ export class DiscussionDetailsComponent implements OnInit {
     }
 
     sendMessage() {
+        this.message.date = new Date();
         this.messageService.save(this.message, this.discussion.id).then((messageSaved) => {
             this.discussion.messages.push(messageSaved);
-        })
+            this.message = new Message(null, this.user, null, "");
+            // recharger la page pour scroll vers le bas et afficher le dernier message -> si trop long en prod, enlever
+            this.ngOnInit();
+        });
     }
 
 }
